012da3b2049987af4c0cfa578a6a13c6251bf696
Vladimir <springfield.3298@gmail.com>
2022-04-29 15:39:24 +0700

    filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilmView, setFilterSort, setActiveFilterSort, setActiveFilterCategory, setActiveFilterGenre } from '../redux/actions/filters';
+import { v4 as uuidv4 } from 'uuid';
 
 import { useHttp } from '../hooks/useHttp';
 
@@ -21,7 +22,6 @@ const Filters = () => {
       { label: "увеличению оценки", name: "upgrade" },
       { label: "убавлению оценки", name: "downgrade" }
    ]);
-
    const [categoryArr, setCategoryArr] = useState([{ "id": 5, "label": "Все", "name": "all" }]);
    const [genreArr, setGenreArr] = useState([{ "id": 8, "label": "Все", "name": "all" }]);
 
@@ -30,41 +30,43 @@ const Filters = () => {
          .then(data => setCategoryArr([...categoryArr, ...data]));
       request('http://localhost:3001/genres')
          .then(data => setGenreArr([...genreArr, ...data]));
+
+      document.body.addEventListener('click', onCloseFilterItem);
    }, []);
 
-   // const [activeSort, setActiveSort] = useState(sortArr[1].label);
 
    const [sortTab, setSortTab] = useState(false);
    const [categoryTab, setCategoryTab] = useState(false);
    const [genreTab, setGenreTab] = useState(false);
 
    const addSortItem = () => {
-      return sortArr.map(({ id, label, name }) => {
+      return sortArr.map(({ label, name }) => {
          return (
             <li
-               key={id}
-               className={'filters__sort-item filters__item'}
+               key={uuidv4()}
+               className={'filters__sort-item filters__item filtab'}
                onClick={() => {
                   dispatch(setActiveFilterSort({ name, label }));
                   dispatch(setFilterSort(name, activeFilterCategory.name, activeFilterGenre.name, filterView, films));
-                  // setActiveSort(label);
+                  setSortTab(false);
                }}
             >
-               <span>{label}</span>
+               {label}
             </li >
          )
       });
    }
 
    const addCategoryItem = () => {
-      return categoryArr.map(({ id, name, label }) => {
+      return categoryArr.map(({ name, label }) => {
          return (
             <li
-               key={id}
+               key={uuidv4()}
                className={'filters__category-item filters__item'}
                onClick={() => {
                   dispatch(setActiveFilterCategory({ name, label }));
                   dispatch(setFilterSort(activeFilterSort.name, name, activeFilterGenre.name, filterView, films));
+                  setCategoryTab(false);
                }}
             >
                <span>{label}</span>
@@ -74,20 +76,37 @@ const Filters = () => {
    }
 
    const addGenreItem = () => {
-      return genreArr.map(({ id, name, label }) => {
+      return genreArr.map(({ name, label }) => {
          return (
             <li
-               key={id}
+               key={uuidv4()}
                className={'filters__genre-item filters__item'}
                onClick={() => {
                   dispatch(setActiveFilterGenre({ name, label }));
                   dispatch(setFilterSort(activeFilterSort.name, activeFilterCategory.name, name, filterView, films));
+                  setGenreTab(false);
                }}
             >
                <span>{label}</span>
             </li >
          )
       });
+   }
+
+   const refSort = useRef();
+   const refCategory = useRef();
+   const refGenre = useRef();
+
+   const onCloseFilterItem = (e) => {
+      if (!e.path.includes(refSort.current)) {
+         setSortTab(false);
+      }
+      if (!e.path.includes(refCategory.current)) {
+         setCategoryTab(false);
+      }
+      if (!e.path.includes(refGenre.current)) {
+         setGenreTab(false);
+      }
    }
 
    const sortItemArr = addSortItem();
@@ -115,38 +134,44 @@ const Filters = () => {
             </div>
          </div>
          <div className="filters">
-            <div className="filters__sort">
-               <p>
-                  Сортировать по
-                  <span onClick={() => setSortTab(!sortTab)}>
-                     {activeFilterSort.label}
-                  </span>
-               </p>
-               <ul className='filters__sort-list filters__list'>
-                  {sortTab ? sortItemArr : null}
-               </ul>
+            <div className="filters__sort" >
+               <div ref={refSort}>
+                  <p>
+                     Сортировать по
+                     <span className='filtab' onClick={() => setSortTab(!sortTab)}>
+                        {activeFilterSort.label}
+                     </span>
+                  </p>
+                  <ul className='filters__sort-list filters__list'>
+                     {sortTab ? sortItemArr : null}
+                  </ul>
+               </div>
             </div>
             <div className="filters__category">
-               <p>
-                  Категория
-                  <span onClick={() => setCategoryTab(!categoryTab)}>
-                     {activeFilterCategory.label}
-                  </span>
-               </p>
-               <ul className='filters__category-list filters__list'>
-                  {categoryTab ? categoryItemArr : null}
-               </ul>
+               <div ref={refCategory}>
+                  <p>
+                     Категория
+                     <span onClick={() => setCategoryTab(!categoryTab)}>
+                        {activeFilterCategory.label}
+                     </span>
+                  </p>
+                  <ul className='filters__category-list filters__list'>
+                     {categoryTab ? categoryItemArr : null}
+                  </ul>
+               </div>
             </div>
             <div className="filters__genre">
-               <p>
-                  Жанр
-                  <span onClick={() => setGenreTab(!genreTab)}>
-                     {activeFilterGenre.label}
-                  </span>
-               </p>
-               <ul className='filters__genre-list filters__list'>
-                  {genreTab ? genreItemArr : null}
-               </ul>
+               <div ref={refGenre}>
+                  <p>
+                     Жанр
+                     <span onClick={() => setGenreTab(!genreTab)}>
+                        {activeFilterGenre.label}
+                     </span>
+                  </p>
+                  <ul className='filters__genre-list filters__list'>
+                     {genreTab ? genreItemArr : null}
+                  </ul>
+               </div>
             </div>
          </div>
       </>
