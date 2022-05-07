628626c40882bd4582924dcdb10433119467d16f
Vladimir <springfield.3298@gmail.com>
2022-05-07 07:23:30 +0300

    main

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import { createStore, compose, applyMiddleware } from "redux";
 import reducer from "./reducers";
 import ReduxThunk from 'redux-thunk';
 
-const store = createStore(reducer, compose(applyMiddleware(ReduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducer, compose(applyMiddleware(ReduxThunk)));
 
 export default store;
